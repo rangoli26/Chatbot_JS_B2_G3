82af00be9baf5280b884e26c9305bb6347b25ae1
MrLango <shourya0302@gmail.com>
2019-08-05 13:17:44 +0530

    Minor updates

--- ChatBot/AutoTestBuild.bat
+++ ChatBot/AutoTestBuild.bat
@@ -1,4 +1,4 @@
 @echo off
 ::java -cp C:\Users\320052128\Downloads\Json\ChatBot\Chatbot_JS_B2_G3\ChatBot\target\ChatBot-1.0-SNAPSHOT.jar com.JSB2G3.ChatBot.ChatBot
-mvn clean -q
+mvn clean package -q
 if %errorlevel%==0 (echo "Success") else (echo "Failure")
